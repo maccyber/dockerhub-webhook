e93c8a8ad511e5d4ad82428e28244990a8789f15
Jonas Enge <jonas.enge@t-fk.no>
2017-01-29 21:34:15 +0100

    Better example names

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = {
-  'testhook': 'hello.sh asd 1', // Name of reponame : Script
+  'testhook': 'hello.sh parameter1 parameter2', // Name of reponame : Script
   'maccyber.io': 'maccyber.io.sh',
   'fail': 'fail.sh'
 }
