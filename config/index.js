a9c98276f76ae48f6d84e26a9bf78678fec1f506
Jonas Enge <jonas.enge@t-fk.no>
2017-01-29 23:21:17 +0100

    Update index.js

--- config/index.js
+++ config/index.js
@@ -5,6 +5,6 @@ const envs = process.env
 module.exports = {
   port: envs.SERVER_PORT || 3000,
   route: envs.API_ROUTE || '/api',
-  token: envs.TOKEN || 'abc123'
-  // callbackDisable: envs.CALLBACK_OFF || true // uncomment to deactivate callback
+  token: envs.TOKEN || 'abc123',
+  callbackDisable: envs.CALLBACK_OFF || false // set to true if you want to disable callback to dockerhub
 }
